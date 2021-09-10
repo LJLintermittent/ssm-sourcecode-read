0d99f3db433154d9fe81ff14daacec591093685d
lijiale <1157221853@qq.com>
2021-09-10 22:55:15 +0800

    mybatis核心需求

--- src/main/java/org/apache/ibatis/learn/mybatis/BlogMapper.java
+++ src/main/java/org/apache/ibatis/learn/mybatis/BlogMapper.java
@@ -10,6 +10,13 @@ import org.apache.ibatis.annotations.Param;
  * @author 李佳乐
  * @version 1.0
  */
+@SuppressWarnings("all")
 public interface BlogMapper {
+
+    /**
+     * DAO接口，就是常说的mapper接口，通过观察xml文件的写法，mybatis需要将dao接口的全限定名写在mapper标签的namespace中
+     * 而里面的每个sql语句标签，比如select标签，需要将这个接口的一个方法名字写在id上，mybatis通过全限定名+方法名，也就是namespace+id
+     * 的方式来准确定位一个接口中的某个方法，所以dao接口中的方法是不可以重载的
+     */
     Department selectDeptById(@Param("id") Integer id);
 }
--- src/main/java/org/apache/ibatis/learn/mybatis/MybatisTest.java
+++ src/main/java/org/apache/ibatis/learn/mybatis/MybatisTest.java
@@ -21,6 +21,19 @@ import javax.sql.DataSource;
  */
 public class MybatisTest {
     public static void main(String[] args) {
+
+        /**
+         * orm对象关系映射框架，在我的理解中主要就是完成三项工作，分别是三个映射：
+         * 1.将映射文件中的sql语句与映射接口中的抽象方法做一个映射
+         * 2.sql语句中的参数与方法的入参做一个映射
+         * 3.sql语句的返回结果与方法的返回结果做一个映射
+         *
+         * 基于这三种映射，可以总结出mybatis的核心功能：
+         * 1，将xml文件中的带各种if foreach复杂标签的sql语句解析为干净的sql语句
+         * 2.将数据库操作节点和映射接口中的抽象方法进行绑定，在抽象方法被调用的时候执行数据库操作
+         * 3.将方法的入参转换为sql语句的占位符参数
+         * 4.将sql语句的返回结果封装成对象
+         */
         DataSource dataSource = getDataSource();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
